adee3c8606b00281de49055bc56c6ea8b7af8bc8
dudyrindmitriy <dvdudyrin@mail.ru>
2024-01-19 22:21:27 +0300

    commit 9

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function showOne($id)
+    {
+        $pages = [
+            1 => 'страница 1',
+            2 => 'страница 2',
+            3 => 'страница 3',
+            4 => 'страница 4',
+            5 => 'страница 5',
+        ];
+        if (array_key_exists($id, $pages)) {
+            return $pages[$id];
+        } else {
+            return "Вы ввели некорректную страницу";
+        }
+        return '$id';
+    }
+    public function showAll()
+    {
+        return "showAll";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//9.1
+Route::get('/user/{id?}', function ($id = 0) {
+    return "Передан параметр $id";
+});
+
+//9.2
+Route::get('/{year}/{month}/{day}', function ($year, $month, $day) {
+    $date = $year . '-' . $month . '-' . $day;
+    $wdays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    $dayNumber = date('w', strtotime($date));
+    return $wdays[$dayNumber];
+})->where([
+    'year' => '^[0-9]{4}$',
+    'month' => "^[0-1][0-2]$",
+    'day' => "^[0-3][0-9]$"
+]);
+
+//9.3
+Route::get('/names/{name}', function ($name) {
+    $users = [
+        'user1' => 'city1',
+        'user2' => 'city2',
+        'user3' => 'city3',
+        'user4' => 'city4',
+        'user5' => 'city5'
+    ];
+    if (array_key_exists($name, $users)) {
+        return $users[$name];
+    } else {
+        return "Неверное имя";
+    }
+});
+
+//9.4
+Route::get('/pages/all', [PageController::class, 'showAll']);
+Route::get('/pages/show/{id}', [PageController::class, 'showOne']);
